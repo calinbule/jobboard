d8e9dd68818f0dd776ee767917e9026bfe8cc1f0
Calin Bule <calin.bule@gmail.com>
2021-02-21 18:10:37 +0200

    added fontawesome notificationn icon, corrected message routing

--- apps/userprofile/views.py
+++ apps/userprofile/views.py
@@ -9,24 +9,29 @@ from apps.notification.utilities import create_notification
 def dashboard(request):
     return render(request, 'userprofile/dashboard.html', {'userprofile': request.user.userprofile})
 
+
 @login_required
 def view_application(request, application_id):
     if request.user.userprofile.is_employer:
         application = get_object_or_404(Application, pk=application_id, job__created_by=request.user)
     else:
         application = get_object_or_404(Application, pk=application_id, created_by=request.user)
-    
+
     if request.method == 'POST':
         content = request.POST.get('content')
 
         if content:
-            conversationmessage = ConversationMessage.objects.create(application=application, content=content, created_by=request.user)
+            conversationmessage = ConversationMessage.objects.create(application=application, content=content, created_by = request.user)
 
-            create_notification(request, application.created_by, 'message', extra_id=application.id)
+            if request.user.userprofile.is_employer:
+                create_notification(request, application.created_by, 'message', extra_id=application.id)
+            else:
+                create_notification(request, application.job.created_by, 'message', extra_id=application.id)
 
-            return redirect('view_application', application_id=application_id)
-    
+            return  redirect('view_application', application_id=application_id)
+
     return render(request, 'userprofile/view_application.html', {'application': application})
+
 
 @login_required
 def view_dashboard_job(request, job_id):
